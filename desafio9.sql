cd480547e7b41fcf5f86dc87aec7db9fcef70ec3
Renanbfreitas <renanbf1992@outlook.com>
2023-01-27 09:43:03 -0300

    feat: add req 9

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT 
+COUNT(H.musica_id) AS quantidade_musicas_no_historico
+FROM
+SpotifyClone.historico_reproducoes AS H
+INNER JOIN
+SpotifyClone.usuarios AS U ON U.usuario_id = H.usuario_id
+WHERE U.nome = "Barbara Liskov";
